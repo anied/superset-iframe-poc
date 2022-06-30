791d25a8bdeafae7f9886fceed0bb02e655ab5b0
Alex Nied <alex.nied@spr.com>
2022-06-30 10:50:06 -0500

    WIP- adds embed code to useEffect in <SupersetDashboard/>; 404ing
    Code is successfully mounting an iFrame to the DOM, but it contains
    only a 404 JSON response payload.  Additionally, there is an error
    regarding `Buffer` not being defined.  I believe these *may* be two
    separate and unrelated issues; my suspicion is:
    
    - The Buffer issue is because the embedded SDK is not intended for
      browser usage, and can hopefully be resolved with a polyfill for
      the Buffer object
    - While it is possible the 404 is due to the Buffer issue, I think
      instead it might be due to missing configuration for the Flask
      process to allow/enable embedded dashboards.
    
    These will all be targeted in subsequent commits.  Once a working
    POC of the dashboard alone is complete, proper plumbing for
    fetching tokens will be implemented before moving on to the
    bidirectional interactions.

--- src/components/SupersetDashboard/SupersetDashboard.js
+++ src/components/SupersetDashboard/SupersetDashboard.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { embedDashboard } from "@superset-ui/embedded-sdk";
 
 import './SupersetDashboard.css';
 
 const SupersetDashboard = () => {
+    useEffect(() => {
+        console.log('embedding dashboard');
+        embedDashboard({
+            id: "49c54e0e-9b36-4479-8c00-003e3bc4ae83", // given by the Superset embedding UI
+            supersetDomain: "http://localhost:9000",
+            mountPoint: document.getElementById("dashboard-mount-point"), // any html element that can contain an iframe
+            fetchGuestToken: () => Promise.resolve('--guest_token--'),
+            dashboardUiConfig: { hideTitle: true }, // dashboard UI config: hideTitle, hideTab, hideChartControls (optional)
+        });
+    })
+
     return (
-        <div className="superset-dashboard flex-grow-1">
+        <div id="dashboard-mount-point" className="superset-dashboard flex-grow-1">
             <h2>Superset Dashboard</h2>
         </div>
     );
