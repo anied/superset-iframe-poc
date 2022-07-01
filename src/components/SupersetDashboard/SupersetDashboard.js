5d1b6ec060f640e1cc7e5dfdc90e6b6afa923e14
Alex Nied <alex.nied@spr.com>
2022-07-01 15:15:58 -0500

    Styles dashboard iframe to fill the space appropriately
    This apparently is working with the hardcoded credentials and guest
    pass even a day later.  That said, it might still be worthwhile to
    put some basic automatic auth in place for others that may be using
    this repo as an example (with messaging explaining it is not to be
    used this way in production).

--- src/components/SupersetDashboard/SupersetDashboard.js
+++ src/components/SupersetDashboard/SupersetDashboard.js
@@ -7,17 +7,17 @@ const SupersetDashboard = () => {
     useEffect(() => {
         console.log('embedding dashboard');
         embedDashboard({
-            id: "49c54e0e-9b36-4479-8c00-003e3bc4ae83", // given by the Superset embedding UI
+            id: "e30ac258-c73c-462f-b42b-036858680bd6", // given by the Superset embedding UI
             supersetDomain: "http://localhost:9000",
             mountPoint: document.getElementById("dashboard-mount-point"), // any html element that can contain an iframe
-            fetchGuestToken: () => Promise.resolve('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImZpcnN0X25hbWUiOiJQZXRlciIsImxhc3RfbmFtZSI6Ikxlc2VyIiwidXNlcm5hbWUiOiJndWVzdCJ9LCJyZXNvdXJjZXMiOlt7ImlkIjoiNDljNTRlMGUtOWIzNi00NDc5LThjMDAtMDAzZTNiYzRhZTgzIiwidHlwZSI6ImRhc2hib2FyZCJ9XSwicmxzX3J1bGVzIjpbXSwiaWF0IjoxNjU2NjA1MzY2LjY2NDEyMTIsImV4cCI6MTY1NjYwNTY2Ni42NjQxMjEyLCJhdWQiOiJodHRwOi8vMC4wLjAuMDo4MDgwLyIsInR5cGUiOiJndWVzdCJ9.MU0yRxY5sTRdSmlH4Sx9aqSbmf0TnpOaQQ46aKWndSM'),
+            fetchGuestToken: () => Promise.resolve('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7Imxhc3RfbmFtZSI6Ikxlc2VyIiwiZmlyc3RfbmFtZSI6IlBldGVyIiwidXNlcm5hbWUiOiJndWVzdCJ9LCJyZXNvdXJjZXMiOlt7ImlkIjoiNDljNTRlMGUtOWIzNi00NDc5LThjMDAtMDAzZTNiYzRhZTgzIiwidHlwZSI6ImRhc2hib2FyZCJ9XSwicmxzX3J1bGVzIjpbXSwiaWF0IjoxNjU2NjExODI2LjU0OTc5NTIsImV4cCI6MTY1NjYxMjEyNi41NDk3OTUyLCJhdWQiOiJodHRwOi8vMC4wLjAuMDo4MDgwLyIsInR5cGUiOiJndWVzdCJ9.1o7ni000WE84hwZJFye5YBjnusYqZr3nT1_BtntKeZ0'),
             dashboardUiConfig: { hideTitle: true }, // dashboard UI config: hideTitle, hideTab, hideChartControls (optional)
         });
     })
 
     return (
-        <div id="dashboard-mount-point" className="superset-dashboard flex-grow-1">
-            <h2>Superset Dashboard</h2>
+        <div id="dashboard-mount-point" className="superset-dashboard flex-grow-1 d-flex">
+            <p>Loading... please wait...</p>
         </div>
     );
 };
